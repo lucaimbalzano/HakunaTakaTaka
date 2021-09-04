402fb4deb0ec8801729bc7accf576af4fa82fe04
luca <lucaimbalzano@gmail.com>
2021-09-04 11:16:36 +0200

    ##Add /home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  leftTitle :string= 'Making a World Cleaned';
+  leftSubtitle :string= 'Garbage is a great resource in the wrong place';
+  leftSubtitleTwo :string= 'that lacks someone'+'`'+'s imagination to be recycled for the benefit of all of us.';
   constructor() { }
 
   ngOnInit(): void {
+   
+    
   }
+
+  
+  
 
 }
---
+++ src/assets/js/autoWrite.js
@@ -0,0 +1,13 @@
+const text = "Making a World Cleaned"
+
+let index = 0
+
+function start() {
+    document.body.innerText = text.slice(0, index)
+
+    index++;
+    if (index > text.length - 1) {
+        index = 0
+    }
+}
+setInterval(start, 100)
